fd0b271378ce2331292b89fadfaf824657511428
Naren's Bumcrack  - Lucas Havenkvist <Luhavenkvist@yahoo.dk>
2023-02-24 13:37:45 +0100

    Login metode burde være klar når database bliver sat på, samt Dbsql klassen er lavet.Samt de sidste fixes på GUI til login

--- FrisorSalon/src/main/java/com/example/frisorsalon/Kunde.java
+++ FrisorSalon/src/main/java/com/example/frisorsalon/Kunde.java
@@ -1,4 +1,5 @@
 package com.example.frisorsalon;
 
 public class Kunde {
+
 }
--- FrisorSalon/src/main/java/com/example/frisorsalon/LogInd.java
+++ FrisorSalon/src/main/java/com/example/frisorsalon/LogInd.java
@@ -7,10 +7,15 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import java.sql.Connection;
 
 import javax.swing.*;
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 
 public class LogInd {
@@ -21,18 +26,50 @@ public class LogInd {
     public static Stage primaryStage;
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Label LoginFailed;
 
     public void LogindKnap(ActionEvent event) throws IOException {
-        if (checkLogin == true) {
-            root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
-            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
+
+        if (username.getText().isBlank() == false && password.getText().isBlank() == false){
+            LoginFailed.setText("logging in bitch!");
+            validateLogin();
+        }else {
+            LoginFailed.setText("You fucked up bitch, try again");
         }
+
+
+
+
+
         /*
         StageController.changeScene("hello-view.fxml");
 */
+    }
+
+    public void validateLogin(){
+        Dbsql connectNow = new Dbsql();
+        Connection connectionDB = connectNow.getConnection();
+        String verifyLogin = "SELECT count(1) FROM UserAccounts WHERE username = '" + username.getText() + "' AND password = '" + password.getText() + "'";
+        try{
+            Statement statement = connectionDB.createStatement();
+            ResultSet queryResult = statement.executeQuery(verifyLogin);
+
+            while(queryResult()){
+                if (queryResult.getInt(1) ==1){
+                    LoginFailed.setText("Welcome!");
+
+                }else {
+                    LoginFailed.setText("Invalid login please try again.");
+                }
+            }
+        } catch (Exception e){
+                e.printStackTrace();
+        }
     }
 
     private void checkLogin() throws IOException{
