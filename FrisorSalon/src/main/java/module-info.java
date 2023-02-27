3d913af2dad29f8560d23c1431467c6b2eac0eac
Mario <mariocandalea@gmail.com>
2023-02-27 10:34:31 +0100

    database

--- FrisorSalon/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.frisorsalon {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.frisorsalon to javafx.fxml;
-    exports com.example.frisorsalon;
-}
